f1025c6c3de3ed3b77fbae50d849a8e58b240733
twoods01 <twoods0129@gmail.com>
2018-07-09 21:09:53 -0700

    Add more eye options

--- src/parts/Eyes.jsx
+++ src/parts/Eyes.jsx
@@ -62,27 +62,135 @@ const verticalLines = [
         shape: 'line',
         properties: {
             x1: image => `${image.anchors.eyes.left.center.x}`,
-            y1: image => `${image.anchors.eyes.left.center.y-10}`,
+            y1: image => `${image.anchors.eyes.left.center.y - 10}`,
             x2: image => `${image.anchors.eyes.left.center.x}`,
-            y2: image => `${image.anchors.eyes.left.center.y+10}`
+            y2: image => `${image.anchors.eyes.left.center.y + 10}`
         }
     },
     {
         shape: 'line',
         properties: {
             x1: image => `${image.anchors.eyes.right.center.x}`,
-            y1: image => `${image.anchors.eyes.right.center.y-10}`,
+            y1: image => `${image.anchors.eyes.right.center.y - 10}`,
             x2: image => `${image.anchors.eyes.right.center.x}`,
-            y2: image => `${image.anchors.eyes.right.center.y+10}`
+            y2: image => `${image.anchors.eyes.right.center.y + 10}`
         }
     }
+]
+
+const angryEyes = [
+    {
+        shape: 'line',
+        properties: {
+            x1: image => `${image.anchors.eyes.left.center.x - 10}`,
+            y1: image => `${image.anchors.eyes.left.center.y - 10}`,
+            x2: image => `${image.anchors.eyes.left.center.x + 10}`,
+            y2: image => `${image.anchors.eyes.left.center.y + 10}`,
+        }
+    },
+    {
+        shape: 'line',
+        properties: {
+            x1: image => `${image.anchors.eyes.right.center.x + 10}`,
+            y1: image => `${image.anchors.eyes.right.center.y - 10}`,
+            x2: image => `${image.anchors.eyes.right.center.x - 10}`,
+            y2: image => `${image.anchors.eyes.right.center.y + 10}`,
+        }
+    }
+]
+
+const backSlant = [
+    {
+        shape: 'line',
+        properties: {
+            x1: image => `${image.anchors.eyes.left.center.x - 10}`,
+            y1: image => `${image.anchors.eyes.left.center.y + 10}`,
+            x2: image => `${image.anchors.eyes.left.center.x + 10}`,
+            y2: image => `${image.anchors.eyes.left.center.y - 10}`,
+        }
+    },
+    {
+        shape: 'line',
+        properties: {
+            x1: image => `${image.anchors.eyes.right.center.x + 10}`,
+            y1: image => `${image.anchors.eyes.right.center.y + 10}`,
+            x2: image => `${image.anchors.eyes.right.center.x - 10}`,
+            y2: image => `${image.anchors.eyes.right.center.y - 10}`,
+        }
+    }
+]
+
+const ovals = [
+    {
+        shape: 'ellipse',
+        properties: {
+            cx: image => `${image.anchors.eyes.left.center.x}`,
+            cy: image => `${image.anchors.eyes.left.center.y}`,
+            rx: "10",
+            ry: "5"
+        }
+    },
+    {
+        shape: 'ellipse',
+        properties: {
+            cx: image => `${image.anchors.eyes.right.center.x}`,
+            cy: image => `${image.anchors.eyes.right.center.y}`,
+            rx: "10",
+            ry: "5"
+        }
+    }
+]
+
+const openAndClosed = [
+    twoCircles[0],
+    twoDots[1]
+]
+
+const twoTriangles = [
+    {
+        shape: 'polygon',
+        properties: {
+            points: image => {
+                let botLeft = `${image.anchors.eyes.left.center.x - 5},${image.anchors.eyes.left.center.y + 5}`;
+                let top = `${image.anchors.eyes.left.center.x},${image.anchors.eyes.left.center.y - 5}`;
+                let botRight = `${image.anchors.eyes.left.center.x + 5},${image.anchors.eyes.left.center.y + 5}`;
+
+                return `${botLeft} ${top} ${botRight}`;
+            }
+        }
+    },
+    {
+        shape: 'polygon',
+        properties: {
+            points: image => {
+                let botLeft = `${image.anchors.eyes.right.center.x - 5},${image.anchors.eyes.right.center.y + 5}`;
+                let top = `${image.anchors.eyes.right.center.x},${image.anchors.eyes.right.center.y - 5}`;
+                let botRight = `${image.anchors.eyes.right.center.x + 5},${image.anchors.eyes.right.center.y + 5}`;
+
+                return `${botLeft} ${top} ${botRight}`;
+            }
+        }
+    }
+]
+
+const asterisks = [
+    angryEyes[0],
+    backSlant[0],
+    angryEyes[1],
+    backSlant[1]
 ]
 
 const eyes = [
     twoCircles,
     twoDots,
     horizontalLines,
-    verticalLines
+    verticalLines,
+    angryEyes,
+    backSlant,
+    ovals,
+    openAndClosed,
+    twoTriangles,
+    asterisks
 ]
 
 export default eyes;
