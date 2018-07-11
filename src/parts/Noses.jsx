007249b161c24e9372e10cd617c22ead2e1a1e46
twoods01 <twoods0129@gmail.com>
2018-07-10 21:14:53 -0700

    Add more noses

--- src/parts/Noses.jsx
+++ src/parts/Noses.jsx
@@ -30,10 +30,62 @@ const oval = {
     }
 }
 
+const none = []
+
+const sidewaysTriangle = {
+    shape: 'polyline',
+    properties: {
+        points: image => {
+            const startPoint = `${image.anchors.nose.center.x - 8},${image.anchors.nose.center.y - 10}`;
+            const midPoint = `${image.anchors.nose.center.x + 8},${image.anchors.nose.center.y + 5}`;
+            const endPoint = `${image.anchors.nose.center.x - 8},${image.anchors.nose.center.y + 10}`;
+            return `${startPoint} ${midPoint} ${endPoint}`;
+        }
+    }
+}
+
+const curvedSideways = {
+    shape: 'path',
+    properties: {
+        d: image => {
+            const startPoint = `${image.anchors.nose.center.x - 8} ${image.anchors.nose.center.y - 10}`;
+            const noseHeight = 15;
+            const noseWidth = 16;
+            const relativeMidInflection = `${noseWidth * 2} ${noseHeight}`;
+            const relativeEndPoint = `0 ${noseHeight}`;
+
+            return `M ${startPoint} q ${relativeMidInflection} ${relativeEndPoint}`
+        }
+    }
+}
+
+const twoDots = [
+    {
+        shape: 'circle',
+        properties: {
+            r: 2,
+            cx: image => `${image.anchors.nose.center.x - 8}`,
+            cy: image => `${image.anchors.nose.center.y}`
+        }
+    },
+    {
+        shape: 'circle',
+        properties: {
+            r: 2,
+            cx: image => `${image.anchors.nose.center.x + 8}`,
+            cy: image => `${image.anchors.nose.center.y}`
+        }
+    }
+]
+
 const noses = [
     dot,
     triangle,
-    oval
+    oval,
+    none,
+    sidewaysTriangle,
+    curvedSideways,
+    twoDots
 ]
 
 export default noses;
