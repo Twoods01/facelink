6a5e45f62d0680cff4f628b2e9b96842fc2edd99
ddemas <daniellefdemas@gmail.com>
2018-07-10 21:25:53 -0700

    Added 3 new noses and a pair of glasses

--- src/parts/Noses.jsx
+++ src/parts/Noses.jsx
@@ -30,10 +30,63 @@ const oval = {
     }
 }
 
+const halfTriangle = [
+    {
+        shape: 'line',
+        properties: {
+            x1: image => `${image.anchors.nose.center.x}`,
+            y1: image => `${image.anchors.nose.center.y - 40}`,
+            x2: image => `${image.anchors.nose.center.x + 19}`,
+            y2: image => `${image.anchors.nose.center.y + 1}`,
+        }
+    },
+    {
+        shape: 'line',
+        properties: {
+            x1: image => `${image.anchors.nose.center.x + 20}`,
+            y1: image => `${image.anchors.nose.center.y}`,
+            x2: image => `${image.anchors.nose.center.x}`,
+            y2: image => `${image.anchors.nose.center.y}`,
+        }
+    }
+]
+
+const twoDots = [
+    {
+        shape: 'circle',
+        properties: {
+            r: 1,
+            cx: image => `${image.anchors.nose.center.x - 10}`,
+            cy: image => `${image.anchors.nose.center.y}`
+        }
+    },
+    {
+        shape: 'circle',
+        properties: {
+            r: 1,
+            cx: image => `${image.anchors.nose.center.x + 10}`,
+            cy: image => `${image.anchors.nose.center.y}`
+        }
+    }
+]
+
+const pig = twoDots.concat([{
+    shape: 'ellipse',
+    properties: {
+        cx: image => `${image.anchors.nose.center.x}`,
+        cy: image => `${image.anchors.nose.center.y}`,
+        rx: 20,
+        ry: 15
+    }
+}]);
+
 const noses = [
     dot,
     triangle,
-    oval
+    oval,
+    halfTriangle,
+    twoDots,
+    pig
 ]
 
 export default noses;
