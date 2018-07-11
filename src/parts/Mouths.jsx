276e857ce07354f16e3e5ffd2e8f5be95f71754c
twoods01 <twoods0129@gmail.com>
2018-07-10 21:15:16 -0700

    Add more mouths

--- src/parts/Mouths.jsx
+++ src/parts/Mouths.jsx
@@ -49,11 +49,72 @@ const flat = {
     }
 }
 
+const tongueOut = [
+    flat,
+    {
+        shape: 'path',
+        properties: {
+            d: image => {
+                const startPoint = `${image.anchors.mouth.right.x} ${image.anchors.mouth.right.y}`
+                const mouthLength = image.anchors.mouth.right.x - image.anchors.mouth.left.x;
+                const totalYOffset = 50;
+                const relativeMidInflection = `0 ${totalYOffset}`;
+                const relativeEndPoint = `-${mouthLength / 3} 0`;
+                return `M ${startPoint} q ${relativeMidInflection} ${relativeEndPoint}`
+            }
+        }
+    }
+]
+
+const triangle = {
+    shape: 'polygon',
+    properties: {
+        points: image => {
+            const topLeft = `${image.anchors.mouth.left.x},${image.anchors.mouth.left.y - 5}`;
+            const topRight = `${image.anchors.mouth.right.x},${image.anchors.mouth.right.y - 5}`;
+            const mouthLength = image.anchors.mouth.right.x - image.anchors.mouth.left.x;
+            const bottom = `${image.anchors.mouth.left.x + (mouthLength / 2)},${image.anchors.mouth.left.y + 10}`;
+
+            return `${topLeft} ${bottom} ${topRight}`;
+        }
+    }
+}
+
+const smallCircle = {
+    shape: 'circle',
+    properties: {
+        r: 10,
+        cx: image => `${image.anchors.mouth.center.x}`,
+        cy: image => `${image.anchors.mouth.center.y}`
+    }
+}
+
+const D = [
+    flat,
+    {
+        shape: 'path',
+        properties: {
+            d: image => {
+                const startPoint = `${image.anchors.mouth.right.x} ${image.anchors.mouth.right.y}`
+                const mouthLength = image.anchors.mouth.right.x - image.anchors.mouth.left.x;
+                const totalYOffset = 50;
+                const relativeMidInflection = `-${mouthLength / 2} ${totalYOffset}`;
+                const relativeEndPoint = `-${mouthLength} 0`;
+                return `M ${startPoint} q ${relativeMidInflection} ${relativeEndPoint}`
+            }
+        }
+    }
+]
+
 const mouths = [
     smile,
     frown,
     open,
-    flat
+    flat,
+    tongueOut,
+    triangle,
+    smallCircle,
+    D
 ]
 
 export default mouths;
